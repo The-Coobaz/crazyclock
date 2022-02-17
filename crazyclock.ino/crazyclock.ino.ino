c6fdf885ee5f6fcfd7eafe18994cfd9d40fffd5b
Qbunjo <39863704+Qbunjo@users.noreply.github.com>
2022-02-17 14:25:10 +0100

    added lost library

--- crazyclock.ino/crazyclock.ino.ino
+++ crazyclock.ino/crazyclock.ino.ino
@@ -1,3 +1,4 @@
+#include <ESP8266WiFi.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 
@@ -6,18 +7,21 @@ const char *password = "PASS";
 
 int tH = 0, tM = 0, tS = 0; // ntp data
 int mH, mM, mS;             // crazydata
-unsigned long tick = 1000;  // initial value of tick =1s
 int tick = 1000;  // initial value of tick =1s
 
 unsigned long myMillis;
 
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "tempus1.gum.gov.pl");
 NTPClient timeClient(ntpUDP, "europe.pool.ntp.org",3600,60000);
 
 void setup() {
   Serial.begin(115200);
-@ -24,18 +25,22 @@ void setup() {
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
 
   timeClient.begin();
   whatTime();
@@ -30,7 +34,6 @@ void setup() {
 void loop() {
   checkEncoder();
   ticTac();
-  showMe();
 }
 void whatTime() {
   timeClient.update();
@@ -41,20 +44,18 @@ void whatTime() {
 }
 
 void checkEncoder() {
-@ -46,18 +51,21 @@ void checkEncoder() {
+  // TODO
+  // Here we listen to the rotations of the encoder, modify the tick and check
+  // if encoder is not pressed (then reset and return to ntp time) here we will
+  // need a flag variable showing that the time was altered from NTP. This will
   // be used in showMe
 }
 void ticTac() {
-  // debug output
-  Serial.println((String)mH + ":" + mM + ":" + mS);
   
   // here the clock works
-  myMillis = millis() + tick;
-  if (myMillis = millis() && tick > 0) {
   
   if ((millis() >= myMillis) and tick > 0) {
     mS++;
-  } else if (myMillis = millis() && tick < 0) {
     myMillis = (millis() + tick);
     showMe();
   } else if ((millis() >= myMillis) and tick < 0) {
@@ -63,38 +64,34 @@ void ticTac() {
     showMe();
   } // if our second has passed and tick is minus, decrement;
 
-  if (mS = 60) {
-    mS = mS - 60;
   if (mS == 59) {
     mS = mS - 59;
     if (tick > 0) {
       mM++; // if second has passed and tick is plus, increment minute
     }
-@ -66,8 +74,8 @@ void ticTac() {
+  }
+  if (mS < 0) {
     mS = mS + 60;
     mM--; // if second has passed and tick is minus, decrement minute
   }
-  if (mM = 60) {
-    mM = mM - 60;
   if (mM == 59) {
     mM = mM - 59;
     if (tick > 0) {
       mH++; // if minute has passed and tick is plus, increment hour
     }
-@ -76,13 +84,14 @@ void ticTac() {
+  }
+  if (mM < 0) {
     mM = mM + 59;
     mH--; // if minute has passed and tick is minus, decrement hour
   }
-  if (mH = 24) {
   if (mH == 24) {
     mH - 24;
   }
-  if (mH = -1) {
   if (mH == -1) {
     mH = mH + 24;
   };
 }
 void showMe() {
-  Serial.println((String)mH + ":" + mM + ":" + mS); //debug outpu
+  Serial.println((String)mH + ":" + mM + ":" + mS); //debug output
   // here we show the result on the screen TODO
 }
