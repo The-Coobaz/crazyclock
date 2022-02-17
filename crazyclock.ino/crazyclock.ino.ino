b72a17d1828b26cadd88d7e254ec98b4e28a54e1
Wit <kubiczak.jakub@gmail.com>
2022-02-17 11:50:08 +0100

    formatting updates

--- crazyclock.ino/crazyclock.ino.ino
+++ crazyclock.ino/crazyclock.ino.ino
@@ -33,20 +33,22 @@ void loop() {
 }
 void whatTime() {
   timeClient.update();
-  tH=timeClient.getHours();
-  tM=timeClient.getMinutes();
-  tS=timeClient.getSeconds();
-   
+  tH = timeClient.getHours();
+  tM = timeClient.getMinutes();
+  tS = timeClient.getSeconds();
 }
 
-  void checkEncoder(){
-    //TODO
-      //Here we listen to the rotations of the encoder, modify the tick and check if encoder is not pressed (then reset and return to ntp time)
-      //here we will need a flag variable showing that the time was altered from NTP. This will be used in showMe
-    }
-  void ticTac() {
-    Serial.println((String)mH +":" + mM + ":" + mS);//debug output
-      //here the clock works
+void checkEncoder() {
+  // TODO
+  // Here we listen to the rotations of the encoder, modify the tick and check
+  // if encoder is not pressed (then reset and return to ntp time) here we will
+  // need a flag variable showing that the time was altered from NTP. This will
+  // be used in showMe
+}
+void ticTac() {
+  // debug output
+  Serial.println((String)mH + ":" + mM + ":" + mS);
+  // here the clock works
   myMillis = millis() + tick;
   if (myMillis = millis() && tick > 0) {
     mS++;
@@ -54,31 +56,33 @@ void whatTime() {
     mS--;
   } // if our second has passed and tick is minus, decrement;
 
-    if (mS=60){
-      mS=mS-60;
+  if (mS = 60) {
+    mS = mS - 60;
     if (tick > 0) {
       mM++; // if second has passed and tick is plus, increment minute
-    } }
+    }
+  }
   if (mS < 0) {
     mS = mS + 60;
     mM--; // if second has passed and tick is minus, decrement minute
   }
-    if (mM=60){
-      mM=mM-60;
+  if (mM = 60) {
+    mM = mM - 60;
     if (tick > 0) {
       mH++; // if minute has passed and tick is plus, increment hour
-    } }
+    }
+  }
   if (mM < 0) {
     mM = mM + 59;
     mH--; // if minute has passed and tick is minus, decrement hour
   }
-    if (mH=24){
-      mH-24;
-    }
+  if (mH = 24) {
+    mH - 24;
+  }
   if (mH = -1) {
     mH = mH + 24;
   };
 }
-    void showMe(){
-      //here we show the result on the screen TODO
-    }
+void showMe() {
+  // here we show the result on the screen TODO
+}
