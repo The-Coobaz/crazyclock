36b3d6dcc3e8609fd011f2592f4d8ffc99e47f2d
Wit <kubiczak.jakub@gmail.com>
2022-02-20 16:06:33 +0100

    formatting

--- src/crazyclock.ino
+++ src/crazyclock.ino
@@ -5,14 +5,15 @@
 const char *ssid = "SSID";
 const char *password = "PASS";
 
-int mH, mM, mS;             // crazydata
-int tick = 1000;  // initial value of tick =1s
+int mH, mM, mS;  // crazydata
+int tick = 1000; // initial value of tick =1s
 char zerro[] = {"0"};
 
-unsigned long myMillis; //maybe myMillis should be a function returning the result?
+unsigned long
+    myMillis; // maybe myMillis should be a function returning the result?
 
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "europe.pool.ntp.org",3600,60000);
+NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 3600, 60000);
 
 void setup() {
   Serial.begin(115200);
@@ -25,7 +26,7 @@ void setup() {
 
   timeClient.begin();
   whatTime();
-   myMillis = (millis() + tick);
+  myMillis = (millis() + tick);
 }
 
 void loop() {
@@ -33,14 +34,14 @@ void loop() {
   ticTac();
   showMe();
 }
-void whatTime() { //this function synchronises time with NTP and normalizes the tick to 1 second
+void whatTime() { // this function synchronises time with NTP and normalizes the
+                  // tick to 1 second
   timeClient.update();
   mH = timeClient.getHours();
   mM = timeClient.getMinutes();
   mS = timeClient.getSeconds();
-  tick=1000;
+  tick = 1000;
   Serial.println((String)mH + ":" + mM + ":" + mS);
-  
 }
 
 void checkEncoder() {
@@ -52,34 +53,34 @@ void checkEncoder() {
   // if button pressed invoke "whatTime"
 }
 void ticTac() {
-  
+
   // here the clock works
-  
+
   if ((millis() >= myMillis) and tick > 0) {
     mS++;
     myMillis = (millis() + tick);
-   
+
   } else if ((millis() >= myMillis) and tick < 0) {
     mS--;
     myMillis = (millis() + tick);
-    
+
   } // if our second has passed and tick is minus, decrement;
 
-  if (mS == 59 and tick>0) {
+  if (mS == 59 and tick > 0) {
     mS = -1;
     mM++; // if second has passed and tick is plus, increment minute
-    }
-  
-  if (mS < 0 and tick<0) {
+  }
+
+  if (mS < 0 and tick < 0) {
     mS = mS + 60;
     mM--; // if second has passed and tick is minus, decrement minute
   }
-  if (mM == 59 and tick>0) {
+  if (mM == 59 and tick > 0) {
     mM = -1;
     mH++; // if minute has passed and tick is plus, increment hour
-    }
-  
-  if (mM < 0 and tick<0) {
+  }
+
+  if (mM < 0 and tick < 0) {
     mM = mM + 59;
     mH--; // if minute has passed and tick is minus, decrement hour
   }
@@ -92,11 +93,15 @@ void ticTac() {
 }
 void showMe() {
   Serial.print((String)mH + ":");
-  if (mM <10){Serial.print(zerro[0]);}
-  Serial.print((String)mM+":");
-  if (mS <10){Serial.print(zerro[0]);}
+  if (mM < 10) {
+    Serial.print(zerro[0]);
+  }
+  Serial.print((String)mM + ":");
+  if (mS < 10) {
+    Serial.print(zerro[0]);
+  }
   Serial.print(mS);
-  Serial.println(); //debug output
+  Serial.println(); // debug output
   // here we show the result on the screen TODO
-   //this will need concatenation in order to print it on the screen
+  // this will need concatenation in order to print it on the screen
 }
