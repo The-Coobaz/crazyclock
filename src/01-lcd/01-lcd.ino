35264a5f92f4a121bc04aa97a27a560bb416095b
wiiitek <you@example.com>
2022-04-19 17:38:30 +0200

    exclude 01-lcd from formatting check because of included library

--- src/01-lcd/01-lcd.ino
+++ src/01-lcd/01-lcd.ino
@@ -1,15 +1,15 @@
-#include <Wire.h>
 #include "LiquidCrystal_I2C.h"
+#include <Wire.h>
 
-//initialize the liquid crystal library
-//the first parameter is the I2C address
-//the second parameter is how many rows are on your screen
-//the third parameter is how many columns are on your screen
+// initialize the liquid crystal library
+// the first parameter is the I2C address
+// the second parameter is how many rows are on your screen
+// the third parameter is how many columns are on your screen
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 void setup() {
 
-  //initialize lcd screen
+  // initialize lcd screen
   lcd.init();
   // turn on the backlight
   lcd.backlight();
