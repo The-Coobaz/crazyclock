e0053fea5807580a61a77777d79ce42e5c01e357
Wiiitek <wiiitek@example.com>
2022-09-25 14:36:27 +0200

    Add static constructor method

--- src/PassedTime/PassedTime.cpp
+++ src/PassedTime/PassedTime.cpp
@@ -1,1 +1,10 @@
 #include "PassedTime.h"
+
+PassedTime::PassedTime(long seconds, long millis) {
+    this->_seconds = seconds;
+    this->_millis = millis;
+};
+
+PassedTime PassedTime::fromDistance(long startMillis, long endMillis) {
+  return PassedTime(0L, 0L);
+};
--- src/PassedTime/PassedTime.h
+++ src/PassedTime/PassedTime.h
@@ -1,5 +1,19 @@
 #ifndef CRAZYCLOCK_PASSED_MILLIS_H
 #define CRAZYCLOCK_PASSED_MILLIS_H
 
+class PassedTime {
+
+private:
+    long _seconds;
+    long _millis;
+
+    PassedTime(long seconds, long millis);
+
+public:
+    static PassedTime fromDistance(long startMillis, long endMillis);
+
+    long getSeconds();
+    long getMillis();
+};
 
 #endif
