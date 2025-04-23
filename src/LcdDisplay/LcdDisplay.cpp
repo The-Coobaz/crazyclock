f864b1c7d0ea3d6059b532eca1520396ac2886ae
Your Name <you@example.com>
2025-04-23 22:30:35 +0200

    Updates for LCD Display

--- src/LcdDisplay/LcdDisplay.cpp
+++ src/LcdDisplay/LcdDisplay.cpp
@@ -1,9 +1,28 @@
 #include "LcdDisplay.h"
 
 LcdDisplay::LcdDisplay() {
-  const int LCD_COLS = 16;
-  const int LCD_ROWS = 2;
   Serial.println("Starting LCD...");
   this->lcd = hd44780_I2Cexp();
+  for (int i = 0; i < LcdDisplay::columns; i++) {
+    LcdDisplay::blankLine[i] = ' ';
+  }
+  LcdDisplay::blankLine[LcdDisplay::columns] = '\0';
 
+  int status = this->lcd.begin(LcdDisplay::columns, LcdDisplay::rows);
+  if (status) {
+    Serial.print("LCD error status: ");
+    Serial.println(status);
+    // blink error code using the onboard LED if possible (stops program)
+    hd44780::fatalError(status);
+  } else {
+    Serial.println("LCD Started");
+  }
+
+  lcd.clear();
+};
+
+void LcdDisplay::setLine1(char *line) {
+  this->lcd.setCursor(0, 0);
+  this->lcd.print(LcdDisplay::blankLine);
+  this->lcd.print(line);
 };
--- src/LcdDisplay/LcdDisplay.h
+++ src/LcdDisplay/LcdDisplay.h
@@ -4,10 +4,18 @@
 #include <hd44780.h>
 #include <hd44780ioClass/hd44780_I2Cexp.h>
 
+#include <array>
+#include <string>
+
 class LcdDisplay {
 public:
   LcdDisplay();
 
+  void setLine1(char *line);
+
 private:
+  static const uint8_t columns = 16;
+  static const uint8_t rows = 2;
+  static char blankLine[columns + 1];
   hd44780_I2Cexp lcd;
 };
